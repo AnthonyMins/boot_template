8df98fc5de36cac1801e97a81a1c98be1173b6e1
AnthonyMins <min3234@gmail.com>
2016-07-04 11:58:21 +0900

    Create Anthony

--- boot_template/src/main/java/boot/jpa/template/jpa/repository/UserInterface.java
+++
@@ -1,9 +0,0 @@
-package boot.jpa.template.jpa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import boot.jpa.template.jpa.entity.UserDao;
-
-public interface UserInterface extends JpaRepository<UserDao, Integer> {
-
-}
